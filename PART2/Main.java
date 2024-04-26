3cc2a79f2db408fc59f356b95fec5e98cec9bd24
Daria Gorbunova <daria.gorbunova1@outlook.com>
2024-04-26 22:34:10 +0100

    added method startRaceUI to main class, as per requirements of the specificaiton

--- PART2/Main.java
+++ PART2/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        startRaceGUI();
+    }
+
+    public static void startRaceGUI() {
         GUI gui = new GUI();
         gui.setVisible(true);
     }
