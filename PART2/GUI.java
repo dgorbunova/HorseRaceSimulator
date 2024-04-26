decb057912a27c45888402658aff26b15ee7276b
Daria Gorbunova <daria.gorbunova1@outlook.com>
2024-04-26 21:31:32 +0100

    Imported all libraries, declared panels, created menu bar

--- PART2/GUI.java
+++ PART2/GUI.java
@@ -1,3 +1,47 @@
-public class GUI {
+import javax.swing.*; 
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Hashtable;
+
+
+public class GUI extends JFrame{
     
+    
+    /**
+     * 
+     */
+    public GUI(){
+        setLayout(null); 
+        //declaring pannels used in frame
+        JPanel racePanel = new JPanel(); 
+        JPanel horsePanel = new JPanel();
+        JPanel trackPanel = new JPanel();
+    
+        //menu bar and its components
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBounds(0, 0, 1000, 30);
+
+        JMenu customizeMenu = new JMenu("Horses");
+        JMenuItem viewHorses = new JMenuItem("View Horses");
+
+        JMenu statsMenu = new JMenu("Statistics");
+        JMenuItem statsItem = new JMenuItem("View Statistics");
+
+        JMenu betMenu = new JMenu("Virtual Betting");
+        JMenuItem betItem = new JMenuItem("Place a bet");
+
+        //adding components to menu bar and menu bar to frame
+        betMenu.add(betItem);
+        customizeMenu.add(viewHorses);
+        statsMenu.add(statsItem);
+        menuBar.add(customizeMenu);
+        menuBar.add(statsMenu);
+        menuBar.add(betMenu);
+        add(menuBar);
+        
+    }
 }
