386bc64e7e845c4696e508c289b98b415100a1ec
Daria Gorbunova <daria.gorbunova1@outlook.com>
2024-04-26 21:03:09 +0100

    Completed Horse class

---
+++ PART1/Horse.java
@@ -0,0 +1,85 @@
+
+    /**
+ * Write a description of class Horse here.
+ * This class represents a horse in the race. It contains fields for the name of the horse, the symbol that represents the horse.
+ * 
+ * @author  Daria Gorbunova
+ * @version 20-04-2024
+ */
+public class Horse
+{
+    //Fields of class Horse, set to private, so they can only be accessed by methods of the class
+    private String name;
+    private char symbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double confidence;
+    
+    
+      
+    //Constructor of class Horse
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        this.confidence = horseConfidence;
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        this.hasFallen = true;
+    }
+    
+    public double getConfidence()
+    {
+        return this.confidence;
+    }
+    
+    public int getDistanceTravelled()
+    {
+        return this.distanceTravelled;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public char getSymbol()
+    {
+        return this.symbol;
+    }
+    
+    public void goBackToStart()
+    {
+        this.distanceTravelled = 0;
+    }
+    
+    public boolean hasFallen()
+    {
+        return this.hasFallen;
+    }
+
+    public void moveForward()
+    {
+        this.distanceTravelled= this.distanceTravelled + 1;
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        this.confidence = newConfidence;
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        this.symbol = newSymbol;
+    }
+    
+}
+
+
